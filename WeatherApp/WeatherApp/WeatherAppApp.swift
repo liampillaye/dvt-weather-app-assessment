c0d3912cee63c04bebe184d76e3f96ac1e98045c
Liam Pillaye <liampillaye@Liams-MacBook-Air.local>
2025-10-23 20:21:33 +0200

    Initial commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Liam Pillaye on 2025/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
